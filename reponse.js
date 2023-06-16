b62e99ddb2749520082805c23d49fe77f24a0c88
Mohd Irshad <112839178+irshad9881@users.noreply.github.com>
2023-06-16 20:08:49 +0530

    chatgptcommit

---
+++ reponse.js
@@ -0,0 +1,7 @@
+const responseObj = {
+    Hello: "Hey ! How are you doing ?",
+    Hi: "Hey! What's Up",
+    today: new Date().toDateString(),
+    time: new Date().toLocaleTimeString(),
+  };
+  
